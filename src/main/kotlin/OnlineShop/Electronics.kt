c3f0f5ce5f5d70229f449972bb6e6c58fd89e160
LuciaES <lespsan543@g.educaand.es>
2023-02-23 21:21:28 +0100

    Ejercicio productos terminado con companion object

--- src/main/kotlin/OnlineShop/Electronics.kt
+++
@@ -1,9 +0,0 @@
-package OnlineShop
-
-abstract class Electronics : Produt {
-    var manufacturer = ""
-    constructor(regularInitialPrice:Double, manufact:String):super(regularInitialPrice){
-        regularPrice = regularInitialPrice
-        manufacturer = manufact
-    }
-}

d575767e16a4e5238c65dde8fb038e5ab8e1ee65
LuciaES <lespsan543@g.educaand.es>
2023-02-24 21:29:18 +0100

    Añadida interfaz del ejercicio 3

---
+++ src/main/kotlin/OnlineShop/RealationalInterface.kt
@@ -0,0 +1,9 @@
+package OnlineShop
+
+interface RealationalInterface {
+    abstract fun isGreater(a:Any):Boolean
+
+    abstract fun isLess(a:Any): Boolean
+
+    abstract fun isEqual(a:Any): Boolean
+}
